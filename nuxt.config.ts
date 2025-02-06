d754b13dc398e80a56ee981a3b480b77088831f6
Daiki Sudo <daiksud@proton.me>
2025-02-06 13:47:56 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', 'vuetify-nuxt-module', '@nuxt/image'],
+  modules: ['@nuxt/content', 'vuetify-nuxt-module'],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
     preview: {
       api: 'https://api.nuxt.studio',
     },
+  },
+  nitro: {
+    static: true,
   },
   vuetify: {
     vuetifyOptions: {
